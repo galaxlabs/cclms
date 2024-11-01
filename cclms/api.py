32851348285ad6a746a67f7085e209d5717d47d8
galaxlabs <galaxylab2020@gmail.com>
2024-11-01 09:53:55 +0000

    new Setup

--- cclms/api.py
+++ cclms/api.py
@@ -1,6 +1,27 @@
 import frappe
 import requests
 
+
+
+@frappe.whitelist()
+def get_lead_counts_by_company(company_name):
+    # Define the workflow states to retrieve
+    workflow_states = ["Approved", "Rejected", "Signed", "Pending", "Converted", "Installed","Agreement Sent",]
+    results = []
+
+    # Query the count of leads for each workflow state
+    for state in workflow_states:
+        count = frappe.db.count("ATM Leads", filters={
+            "company": company_name,
+            "workflow_state": state
+        })
+        results.append({
+            "workflow_state": state,
+            "count": count
+        })
+
+    return results
+
 @frappe.whitelist()
 def search_location(query):
     # Example: Dummy data for testing purposes
