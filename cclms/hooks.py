c06b0cea7ff18adeaff7e23546c6a784a5989805
galaxlabs <galaxylab2020@gmail.com>
2026-03-16 23:59:05 +0000

    hook

--- cclms/hooks.py
+++ cclms/hooks.py
@@ -272,7 +272,8 @@ website_route_rules = [
 doc_events = {
     "ATM Leads": {
         "after_save": "cclms.notifications.atm_lead_after_save",
-        "on_trash": "cclms.services.mirror.on_trash_atm_lead.delink_operator_deals"
+        "on_trash": "cclms.services.mirror.on_trash_atm_lead.delink_operator_deals",
+        "after_save": "cclms.utils.communication_utils.handle_atm_lead_workflow"
     },
     "Operator Deal": {
         "before_save": "cclms.services.maintenance.operator_deal_flags.before_save_operator_deal"
